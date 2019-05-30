63451142d4a120a8f7474030b2ea4cff68806925
iamstr <asatar1994@gmail.com>
2019-05-30 04:13:28 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var socket = require('socket.io');
+
+// App setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000,');
+});
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup & pass server
+var io = socket(server);
+
+let handle=[],count;
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    require('getmac').getMac(function(err, macAddress){
+      if (err)  throw err
+      console.log(macAddress)
+  })
+
+
+count++;
+
+  socket.on('disconnect',function (data)  {
+
+
+    handle.filter(elem => {
+      elem !== data.handle
+    });
+
+    console.log( data.handle);
+  });
+
+    socket.on('type',function(data){
+
+        socket.broadcast.emit('type', data);
+    })
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        if(handle.includes(data.handle)){
+
+          return data.handle;
+        }else{
+              handle.push(data.handle)
+
+        }
+        data.onlineUsers=count++;
+        io.sockets.emit('chat', data);
+    });
+
+});
---
+++ public/chat.js
@@ -0,0 +1,73 @@
+// Make connection
+var socket = io.connect('http://localhost:4000');
+
+// Query DOM
+var message = document.getElementById('message'),
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output'),
+      feedback = document.getElementById('feedback')
+      ;
+
+
+handle.addEventListener('click',function(){
+
+if(handle.value!==''){
+
+  handle.disabled = true
+}
+
+})
+
+
+message.addEventListener('keyup',function(event){
+
+  let x = event.which || event.keyCode;
+event.preventDefault();
+  if(event.which === 13 || event.keyCode === 13){
+// alert('enter pressed')
+    socket.emit('chat', {
+      message: message.value,
+      handle: handle.value
+  })
+  message.value = ""
+
+  }
+
+})
+
+
+
+
+
+// Emit events
+btn.addEventListener('click', function(){
+  socket.emit('chat', {
+      message: message.value,
+      handle: handle.value
+  });
+  message.value = "";
+
+});
+
+// Listen for events
+socket.on('chat', function(data){
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+      feedback.innerHTML = '';
+});
+
+
+message.addEventListener('keyup',function(){
+  socket.emit('type', {handle: handle.value})
+  });
+
+
+
+
+
+
+socket.on('type', function(data){
+    feedback.innerHTML = `${data.handle} is typing ...`;
+
+
+});
